816fb2b934c9671aeb764f7fd15d5ba70a1f75d7
Patrick Dilhe <ledilhe@gmail.com>
2026-06-21 23:32:41 -0300

    Corrige palavra usuário e atualiza cache

--- service-worker-v4.js
+++ service-worker-v4.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'check-diario-pwa-v40-funcionarios-layout';
+const CACHE_NAME = 'check-diario-pwa-v41-texto-usuario';
 const ASSET_MANIFEST = './assets/manifest.json';
 const CORE_ASSETS = [
   './',
