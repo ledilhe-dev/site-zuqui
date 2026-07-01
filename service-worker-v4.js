4f0dc2d28f809c977c42765b51ac143d947b30bb
Patrick Dilhe <ledilhe@gmail.com>
2026-07-01 17:04:28 -0300

    Forcar atualizacao do cache das categorias

--- service-worker-v4.js
+++ service-worker-v4.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'check-diario-pwa-v52-restaurar-vencimento-mobile';
+const CACHE_NAME = 'check-diario-pwa-v53-inline-categorias';
 const ASSET_MANIFEST = './assets/manifest.json';
 const CORE_ASSETS = [
   './',
