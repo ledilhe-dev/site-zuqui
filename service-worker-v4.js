2ab7bd2543445cf9f3146456fcde850227029e47
Patrick Dilhe <ledilhe@gmail.com>
2026-07-01 21:16:47 -0300

    Ajustar dashboard mobile

--- service-worker-v4.js
+++ service-worker-v4.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'check-diario-pwa-v53-inline-categorias';
+const CACHE_NAME = 'check-diario-pwa-v54-dashboard-mobile';
 const ASSET_MANIFEST = './assets/manifest.json';
 const CORE_ASSETS = [
   './',
