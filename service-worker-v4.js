0b56cd60a8cc4fa215f9360ba82298ea53aa45c7
Patrick Dilhe <ledilhe@gmail.com>
2026-07-27 22:53:20 -0300

    Corrige enquadramento e sobreposições responsivas

--- service-worker-v4.js
+++ service-worker-v4.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'check-diario-pwa-v114-sem-filtro-lojas';
+const CACHE_NAME = 'check-diario-pwa-v115-enquadramento-global';
 const ASSET_MANIFEST = './assets/manifest.json';
 const CORE_ASSETS = [
   './',
