f803cdb78a01955ce4744f0bb5890f45a172671c
Patrick Dilhe <ledilhe@gmail.com>
2026-07-28 08:36:49 -0300

    Cria layout dedicado para tablet celular e desktop

--- service-worker-v4.js
+++ service-worker-v4.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'check-diario-pwa-v118-nova-conta-campos';
+const CACHE_NAME = 'check-diario-pwa-v119-device-layout';
 const ASSET_MANIFEST = './assets/manifest.json';
 const CORE_ASSETS = [
   './',
