1b5f10d0969bdddb56535504750879e9031e686f
Patrick Dilhe <ledilhe@gmail.com>
2026-07-27 23:06:48 -0300

    Mantem valor e data lado a lado no celular

--- service-worker-v4.js
+++ service-worker-v4.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'check-diario-pwa-v117-nova-conta-layout';
+const CACHE_NAME = 'check-diario-pwa-v118-nova-conta-campos';
 const ASSET_MANIFEST = './assets/manifest.json';
 const CORE_ASSETS = [
   './',
