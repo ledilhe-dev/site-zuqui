413f70641305d90e57e14376100f275e9f7a0562
Patrick Dilhe <ledilhe@gmail.com>
2026-07-27 22:24:53 -0300

    Refaz recebimentos e cofre com layout responsivo

--- service-worker-v4.js
+++ service-worker-v4.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'check-diario-pwa-v108-fornecedor-sem-seta';
+const CACHE_NAME = 'check-diario-pwa-v109-financeiro-responsivo';
 const ASSET_MANIFEST = './assets/manifest.json';
 const CORE_ASSETS = [
   './',
